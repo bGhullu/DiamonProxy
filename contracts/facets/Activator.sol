e9a4ec307069a95dc21a9a96b2b0d33699e2f0b8
B Ghullu <90811950+bGhullu@users.noreply.github.com>
2022-11-01 17:06:22 +0000

    ERC2535

--- contracts/facets/Activator.sol
+++ contracts/facets/Activator.sol
@@ -16,7 +16,11 @@ import "../libraries/SafeCast.sol";
  * for the underlying asset.
  */
 
-contract Activator is Initializable, AccessControlUpgradeable, ReentrancyGuardUpgradeable {
+contract Activator is
+    Initializable,
+    AccessControlUpgradeable,
+    ReentrancyGuardUpgradeable
+{
     struct Account {
         // The total number of unexchanged tokens that an account has deposited into the system
         uint256 unexchangedBalance;
@@ -41,9 +45,17 @@ contract Activator is Initializable, AccessControlUpgradeable, ReentrancyGuardUp
 
     event Deposit(address indexed user, uint256 unexchangedBalance);
 
-    event Withdraw(address indexed user, uint256 unexchangedBalance, uint256 exchangedBalance);
+    event Withdraw(
+        address indexed user,
+        uint256 unexchangedBalance,
+        uint256 exchangedBalance
+    );
 
-    event Claim(address indexed user, uint256 unexchangedBalance, uint256 exchangedBalance);
+    event Claim(
+        address indexed user,
+        uint256 unexchangedBalance,
+        uint256 exchangedBalance
+    );
 
     // @dev The identifier of the role which maintains other roles.
     bytes32 public constant ADMIN = keccak256("ADMIN");
@@ -67,7 +79,10 @@ contract Activator is Initializable, AccessControlUpgradeable, ReentrancyGuardUp
 
     constructor() {}
 
-    function initialize(address _syntheticToken, address _underlyingToken) external initializer {
+    function initialize(address _syntheticToken, address _underlyingToken)
+        external
+        initializer
+    {
         _setupRole(ADMIN, msg.sender);
         _setRoleAdmin(ADMIN, ADMIN);
         _setRoleAdmin(SENTINEL, ADMIN);
@@ -104,30 +119,61 @@ contract Activator is Initializable, AccessControlUpgradeable, ReentrancyGuardUp
     }
 
     function deposit(uint256 amount) external {
-        _updateAccount(UpdateAccount({user: msg.sender, unexchangedBalance: SafeCast.toInt256(amount), exchangedBalance: 0}));
-        TokenUtils.safeTransferFrom(syntheticToken, msg.sender, address(this), amount);
+        _updateAccount(
+            UpdateAccount({
+                user: msg.sender,
+                unexchangedBalance: SafeCast.toInt256(amount),
+                exchangedBalance: 0
+            })
+        );
+        TokenUtils.safeTransferFrom(
+            syntheticToken,
+            msg.sender,
+            address(this),
+            amount
+        );
         emit Deposit(msg.sender, amount);
     }
 
     function withdraw(uint256 amount) external {
-        _updateAccount(UpdateAccount({user: msg.sender, unexchangedBalance: -SafeCast.toInt256(amount), exchangedBalance: 0}));
+        _updateAccount(
+            UpdateAccount({
+                user: msg.sender,
+                unexchangedBalance: -SafeCast.toInt256(amount),
+                exchangedBalance: 0
+            })
+        );
         TokenUtils.safeTransfer(syntheticToken, msg.sender, amount);
-        emit Withdraw(msg.sender, accounts[msg.sender].unexchangedBalance, accounts[msg.sender].exchangedBalance);
+        emit Withdraw(
+            msg.sender,
+            accounts[msg.sender].unexchangedBalance,
+            accounts[msg.sender].exchangedBalance
+        );
     }
 
     function claim(uint256 amount) external {
         _updateAccount(
-            UpdateAccount({user: msg.sender, unexchangedBalance: -SafeCast.toInt256(amount), exchangedBalance: SafeCast.toInt256(amount)})
+            UpdateAccount({
+                user: msg.sender,
+                unexchangedBalance: -SafeCast.toInt256(amount),
+                exchangedBalance: SafeCast.toInt256(amount)
+            })
         );
         TokenUtils.safeTransfer(underlyingToken, msg.sender, amount);
         TokenUtils.safeBurn(syntheticToken, amount);
-        emit Claim(msg.sender, accounts[msg.sender].unexchangedBalance, accounts[msg.sender].exchangedBalance);
+        emit Claim(
+            msg.sender,
+            accounts[msg.sender].unexchangedBalance,
+            accounts[msg.sender].exchangedBalance
+        );
     }
 
     function _updateAccount(UpdateAccount memory param) internal {
         Account storage _account = accounts[param.user];
-        int256 updateUnexchange = int256(_account.unexchangedBalance) + param.unexchangedBalance;
-        int256 updateExchange = int256(_account.exchangedBalance) + param.exchangedBalance;
+        int256 updateUnexchange = int256(_account.unexchangedBalance) +
+            param.unexchangedBalance;
+        int256 updateExchange = int256(_account.exchangedBalance) +
+            param.exchangedBalance;
         if (updateUnexchange < 0 || updateExchange < 0) {
             revert IllegalState();
         }
